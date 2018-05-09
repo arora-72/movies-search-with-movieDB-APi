f04f51907b96a87c3639863420489ce913db857d
arora-72 <indresh.arora2016@vitstudent.ac.in>
2018-05-10 01:02:05 +0530

    basic implementation done, manual fetch done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MovieRow from './movieRow.js'
+import $ from 'jquery'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {}
+    // console.log("this is my initializer")
+
+    // const movies = [
+    //   {id: 0,title: "Avengers: Infinity War",poster_src:"infinity.jpg", overview: "this is the first overview"},
+    //   {id: 1,title: "The avengers",poster_src:"avengers.jpg", overview: "this is the second overview"},
+
+    // ]
+    
+
+    // var moviesRow = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.id)
+    //   const movieRow = <MovieRow  movie={movie}/>
+      
+        
+    //   moviesRow.push(movieRow)
+    // })
+
+    // this.state = {rows: moviesRow}
+
+    this.performSearch()
+
+  }
+  performSearch() {
+    console.log("perform search using movieDB")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=5717a33dc3459f672c1daf12b8907f6a&language=en-US&query=marvel"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("successfully fetched data")
+        // console.log(searchResults)
+        const results = searchResults.results
+        // console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185/" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+         console.log("failed to fetch data")
+      }
+      
+    })
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          hi there from app.js
-        </p>
+      <div>
+          
+          <table className="titleBar">
+            <tbody>
+              <tr>
+              <td>
+                <img alt="app icon" width="50" src = "movir_db_icon.png"/>
+              </td>
+              <td width = "8" />
+              <td>
+                <h1>MoviesDB search</h1>
+              </td>
+              </tr>
+              
+            </tbody>
+          </table>
+
+          <input style={{
+            fontSize: 24,
+            display:  'block',
+            width: "99%",
+            paddingTop: 8,
+            paddingBotton: 8,
+            paddingLeft:16
+          }} placeholder="enter search term"/>
+
+          {this.state.rows}
+
       </div>
     );
   }
